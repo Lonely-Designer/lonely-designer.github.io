90656a6d6aa5dd73a3fbbebdf46503fbaf04d27a
shikakun <k@u-n.jp>
2015-02-19 19:45:42 +0900

    LONELYDESIGNERYO から SHIKAKUN へ Yo できるようにした

--- application.js
+++ application.js
@@ -43,4 +43,22 @@ $(function()
     // Save Tweet Name
     localStorage.setItem('tweetName', $('#entry_1188111055').val());
   });
+
+  $('.yo').click(function() {
+    var http = new XMLHttpRequest(),
+        api_url = 'http://api.justyo.co/yo/',
+        api_token = '4e41850c-2734-4a68-a3b7-93f1c827cee4',
+        username = $(this).attr('data-yo-username'),
+        params = 'api_token=' + api_token + '&username=' + username;
+    http.open('POST', api_url, true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.setRequestHeader('Content-length', params.length);
+    http.setRequestHeader('Connection', 'close');
+    http.onreadystatechange = function() {
+      if(http.readyState == 4 && http.status == 200) {
+        alert('YO送信完了!');
+      }
+    }
+    http.send(params);
+  });
 });
