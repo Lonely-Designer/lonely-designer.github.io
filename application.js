99789eef7565d7ca34583e8d788999564743b772
Okada Yusuke <okadayusuke@PMAC076S.local>
2015-02-20 09:54:35 +0900

    Yo!

--- application.js
+++ application.js
@@ -56,7 +56,7 @@ $(function()
     http.setRequestHeader('Connection', 'close');
     http.onreadystatechange = function() {
       if(http.readyState == 4 && http.status == 200) {
-        alert('YO送信完了!');
+        alert('Yo!');
       }
     }
     http.send(params);
